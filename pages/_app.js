39f8577242e65496c85672b3068a2131e105f3fc
root <root@LAPTOP-P07JSRR3.localdomain>
2022-08-19 11:25:27 +0530

    moralis complete

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import '../styles/globals.css'
 import Header from '../components/Header'
 import { MoralisProvider } from 'react-moralis'
+import { NotificationProvider } from "web3uikit"
 const APP_ID = process.env.NEXT_PUBLIC_APP_ID
 const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
-        <Header></Header>
-        <Component {...pageProps} />
+      <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}  >
+        <NotificationProvider>
+          <Header></Header>
+          <Component {...pageProps} />
+        </NotificationProvider>
       </MoralisProvider >
     </div >)
 }
